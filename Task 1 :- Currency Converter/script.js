12f1c9e55bc7aeb0af7e3e6a0f1fb6582c2da439
BABITA LOHANI <93931565+babita2002@users.noreply.github.com>
2023-09-07 17:11:32 +0530

    index.js

--- Task 1 :- Currency Converter/script.js
+++ Task 1 :- Currency Converter/script.js
@@ -1,42 +1,42 @@
-const select = document.querySelectorAll(".currency");
-const btn = document.getElementById("btn");
-const num = document.getElementById("num");
-const ans = document.getElementById("ans");
+const curItem1 = document.querySelector(".cur-item-1");
+const curItem2 = document.querySelector(".cur-item-2");
+const curInput1 = document.querySelector(".cur-input-1");
+const curInput2 = document.querySelector(".cur-input-2");
 
-fetch("https://api.frankfurter.app/currencies")
-  .then((data) => data.json())
-  .then((data) => {
-    display(data);
-  });
+const rateBox = document.querySelector(".rate-box");
+const changeBtn = document.querySelector(".fa-retweet");
 
-function display(data) {
-  const entries = Object.entries(data);
-  for (var i = 0; i < entries.length; i++) {
-    select[0].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}</option>`;
-    select[1].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}</option>`;
-  }
-}
+function calc() {
+  const curItem1Value = curItem1.value;
+  const curItem2Value = curItem2.value;
 
-btn.addEventListener("click", () => {
-  let currency1 = select[0].value;
-  let currency2 = select[1].value;
-  let value = num.value;
+  fetch(`https://api.exchangerate-api.com/v4/latest/${curItem1Value}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const rate = data.rates[curItem2Value];
 
-  if (currency1 != currency2) {
-    convert(currency1, currency2, value);
-  } else {
-    alert("Choose Diffrent Currency");
-  }
-});
+      rateBox.textContent = `1 ${curItem1Value} = ${rate.toFixed(
+        4
+      )} ${curItem2Value}`;
 
-function convert(currency1, currency2, value) {
-  const host = "api.frankfurter.app";
-  fetch(
-    `https://${host}/latest?amount=${value}&from=${currency1}&to=${currency2}`
-  )
-    .then((val) => val.json())
-    .then((val) => {
-      console.log(Object.values(val.rates)[0]);
-      ans.value = Object.values(val.rates)[0];
+      curInput2.value = (curInput1.value * rate).toFixed(2);
     });
 }
+
+function listeners() {
+  curItem1.addEventListener("change", calc);
+  curItem2.addEventListener("change", calc);
+  curInput1.addEventListener("input", calc);
+  curInput2.addEventListener("input", calc);
+
+  changeBtn.addEventListener("click", () => {
+    [curItem1.value, curItem2.value] = [curItem2.value, curItem1.value];
+    calc();
+    changeBtn.classList.toggle("rotate-btn");
+  });
+}
+
+window.onload = () => {
+  listeners();
+  calc();
+};
